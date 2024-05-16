457c59e455c216e89044fb6f15d39501569513b4
RedziMar <159469403+RedziMar@users.noreply.github.com>
2024-05-16 16:50:08 +0800

    WEW

--- GROUP NI RAF/My Project/Resources.Designer.vb
+++ GROUP NI RAF/My Project/Resources.Designer.vb
@@ -93,6 +93,46 @@ Namespace My.Resources
         '''<summary>
         '''  Looks up a localized resource of type System.Drawing.Bitmap.
         '''</summary>
+        Friend ReadOnly Property pg_6() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("pg 6", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
+        
+        '''<summary>
+        '''  Looks up a localized resource of type System.Drawing.Bitmap.
+        '''</summary>
+        Friend ReadOnly Property pg_9() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("pg 9", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
+        
+        '''<summary>
+        '''  Looks up a localized resource of type System.Drawing.Bitmap.
+        '''</summary>
+        Friend ReadOnly Property pg7() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("pg7", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
+        
+        '''<summary>
+        '''  Looks up a localized resource of type System.Drawing.Bitmap.
+        '''</summary>
+        Friend ReadOnly Property pg8() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("pg8", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
+        
+        '''<summary>
+        '''  Looks up a localized resource of type System.Drawing.Bitmap.
+        '''</summary>
         Friend ReadOnly Property Registration() As System.Drawing.Bitmap
             Get
                 Dim obj As Object = ResourceManager.GetObject("Registration", resourceCulture)
