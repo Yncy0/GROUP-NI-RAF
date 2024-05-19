8098d1a8236f05e08571e4de9bbffa93f4c2e566
Yncy0 <paalayancy@gmail.com>
2024-05-19 10:13:31 +0800

    DialogResult

--- GROUP NI RAF/Form5.vb
+++ GROUP NI RAF/Form5.vb
@@ -1,8 +1,11 @@
 ﻿Public Class Form5
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        Form6.Show()
-        Me.Hide()
-
+        Dim showDialogOptions As DialogResult = MessageBox.Show("The test will contain 10 Items \n you have only 20 mins \n Do you want to proceed?", "WARNING!", MessageBoxButtons.YesNo)
+        If showDialogOptions = DialogResult.Yes Then
+            Form6.Show()
+        ElseIf showDialogOptions = DialogResult.No Then
+            Return
+        End If
     End Sub
 
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
