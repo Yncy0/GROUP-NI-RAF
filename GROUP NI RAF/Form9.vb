92f22e126285992ee51337d9ef6c8fab86ed81e7
Yncy0 <paalayancy@gmail.com>
2024-05-19 13:35:36 +0800

    add MessageBoxDialog

--- GROUP NI RAF/Form9.vb
+++ GROUP NI RAF/Form9.vb
@@ -1,7 +1,11 @@
 ﻿Public Class Form9
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        Form10.Show()
-        Me.Hide()
+        Dim showDialogOptions As DialogResult = MessageBox.Show("The test will contain 10 Items \n you have only 20 mins \n Do you want to proceed?", "WARNING!", MessageBoxButtons.YesNo)
+        If showDialogOptions = DialogResult.Yes Then
+            Form10.Show()
+        ElseIf showDialogOptions = DialogResult.No Then
+            Return
+        End If
 
     End Sub
 
